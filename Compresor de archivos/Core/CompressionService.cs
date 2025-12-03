f1983fe9290e2802ce56d944e01b7dbbe3af77d0
Cotue <jovenegas@estudiantec.cr>
2025-12-03 17:28:32 -0600

    Implementacion basic, huffman base, Falta implementar compress en HuffmanCompresso, falta el decompress en HuffmanCompressor, Asegurar que CompressorFactory tiene el using Compresor_de_archivos.Core.Huffman;, Que CompressionService está usando AlgorithmSelector.Huffman cuando el usuario escoja ese algoritmo., Que la UI llama a CompressionService correctamente. Para ver los nombres de los objetos de la UI pueden hacerlo desde MainForms.cs[Diseno] o al final de MainForm.Designer.cs

---
+++ Compresor de archivos/Core/CompressionService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compresor_de_archivos.Core
+{
+    internal class CompressionService
+    {
+    }
+}
---
+++ Compresor de archivos/Core/GeneralCompressor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compresor_de_archivos.Core
+{
+    public interface GeneralCompressor
+    {
+        string Name { get; }
+
+        byte[] Compress(byte[] input);
+
+        byte[] Decompress(byte[] input);
+    }
+}
